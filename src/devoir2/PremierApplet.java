a1c9072a43d1f92bc7480176d82187c309fcc89c
Jacques Bertrand <jdbertrand@mac.com>
2017-03-06 12:14:40 -0500

    commit message

--- src/devoir2/PremierApplet.java
+++ src/devoir2/PremierApplet.java
@@ -5,7 +5,7 @@ public class PremierApplet extends java.applet.Applet {
 	   // Méthode d'initialisation appelée aprés le chargement dans le navigateur
 	   public void init() {
 	   }
-
+//Commentaire
 	   
 	   public void paint(java.awt.Graphics g){
 	      g.drawString("Allo !", 0, 100);

7091aa2c38e2a8e782a3cb1b69a163e41bbe0f24
Jacques Bertrand <jdbertrand@mac.com>
2017-03-23 15:46:41 -0400

    changes to calendar days

---
+++ src/devoir3/Calendrier.java
@@ -0,0 +1,86 @@
+package devoir3;
+
+import java.awt.*;
+import java.util.GregorianCalendar;
+
+import javax.swing.*;
+
+public class Calendrier extends JApplet {
+	// Variable d'instances
+	GregorianCalendar calendar = new GregorianCalendar();
+	int rangee = 7;
+	int colonne = 7;
+	String[] semaine = {"Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di" };
+	
+	//Interface utilisateur en Applet
+	JLabel[][] grid = new JLabel[rangee][colonne];
+	JLabel jlblHeure = new JLabel("Heure: " + "" + calendar.get(GregorianCalendar.HOUR_OF_DAY) + ":"
+			+ calendar.get(GregorianCalendar.MINUTE));
+	JPanel jpnlCalendar = new JPanel(new GridLayout(7, 7));
+	JPanel jpnlHeure = new JPanel(new GridLayout(1, 1));
+	
+	//Paramètres pour la date d'aujourd'hui en utilisant GregorianCalendar
+	int today = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+	int daysInMonth = calendar.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+	int daysInWeek = calendar.get(GregorianCalendar.DAY_OF_WEEK);
+
+	//Methode d'initialisation
+	public void init() {
+		// setting grid
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				grid[i][j] = new JLabel();
+			}
+
+		}
+		// Tableau 2 dimensions pour la grille 
+		Container contenu = getContentPane();
+		for (int i = 0; i < rangee; i++) {
+			for (int j = 0; j < colonne; j++) {
+				jpnlCalendar.add(grid[i][j]);
+			}
+		}
+		// Ajout des JPanel
+		contenu.add(jpnlCalendar);
+		contenu.add(jpnlHeure, "South");
+		jpnlHeure.add(jlblHeure);
+
+		// draw time
+		if (calendar.get(GregorianCalendar.MINUTE) < 10) {
+			jlblHeure = new JLabel("Heure: " + "" + calendar.get(GregorianCalendar.HOUR_OF_DAY) + ":" + "0"
+					+ calendar.get(GregorianCalendar.MINUTE));
+		}
+		// Dessin de l'entête des demaines
+		for (int i = 0; i < semaine.length; i++) {
+			grid[0][i].setText(semaine[i]);
+		}
+		// conditions for the calendar
+		calendar.set(GregorianCalendar.DAY_OF_MONTH, 1);
+		int daysInWeek = calendar.get(GregorianCalendar.DAY_OF_WEEK) - 1;
+		System.out.print(daysInWeek);
+		if (daysInWeek == 0)
+			daysInWeek = 7;
+		calendar.set(GregorianCalendar.DAY_OF_MONTH, today);
+
+		int days = 1;
+
+		for (int k = daysInWeek - 1; k < colonne; k++) {
+			grid[1][k].setText(days + "");
+			if (days == today)
+				grid[1][k].setText(days + "*");
+			days++;
+		}
+		// Dessin du calendrier dans deux boucles imbriquées
+		for (int i = 2; i <= colonne; i++) {
+			for (int j = 0; j < rangee; j++) {
+				if (days <= daysInMonth) {
+					grid[i][j].setText(days + "");
+					if (days == today)
+						grid[i][j].setText(days + "*");
+					days++;
+				}
+			}
+		}
+
+	}
+}
---
+++ src/devoir3/TicTacToe.java
@@ -0,0 +1,12 @@
+package devoir3;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
